bea78b5704e57685d6901bf2f3e8e5d03b553597
김현진 <hyeonjin.kim@innowireless.com>
2021-06-11 18:50:07 +0900

    [#1] UI 레이아웃 작업

---
+++ frontend/src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import Main from '@/components/Main.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+import LoginForm from '@/components/LoginForm.vue'
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      component: Main
+    },
+    {
+      path: '/hello-world',
+      component: HelloWorld
+    },
+    {
+      path: '/login',
+      component: LoginForm
+    }
+  ]
+})
